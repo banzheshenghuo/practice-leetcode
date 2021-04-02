d4afbdd9e6ce26036f06c43831d2667c672c93d6
shuang.tu@forceclouds.com <2271362435@qq.com>
2021-04-02 09:29:28 +0800

    使用递归反转链表

--- 链表/反转链表-206.js
+++ 链表/反转链表-206.js
@@ -1,3 +1,5 @@
+const {makeListNode}  = require('../工具/链表')
+
 function reverse(head) {
   let prev = null
   let cur = head
@@ -11,3 +13,19 @@ function reverse(head) {
   // 返回反转后的头节点
   return prev
 }
+
+
+function reverseRecursion(head){
+  if(head === null || head.next === null) return head
+
+  const last = reverseRecursion(head.next)
+
+  head.next.next = head
+  head.next = null
+
+  return last
+}
+
+const listNodes = makeListNode([1,2,3,4,5])
+
+reverseRecursion(listNodes)
