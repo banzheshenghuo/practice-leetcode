e1fc6d29702caa6b635ebd729ea57babaa1265cd
shuang.tu@forceclouds.com <2271362435@qq.com>
2021-04-11 10:13:03 +0800

    add 112 114

---
+++ 二叉树/112-路径总和.js
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode-cn.com/problems/path-sum
+ */
+
+// let hasPathSum = function (root, sum) {
+//   if (!root) {
+//     return false
+//   }
+//   // 叶子节点 判断当前的值是否等于 sum 即可
+//   if (!root.left && !root.right) {
+//     return root.val === sum
+//   }
+
+//   return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+// }
---
+++ 二叉树/114-二叉树展开为链表.js
@@ -0,0 +1,4 @@
+/**
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ * 题解 https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by--26/
+ */
