3b21fc335c6ae17d1b236e2ba1f71d6ce1231980
shuang.tu@forceclouds.com <2271362435@qq.com>
2021-04-30 10:11:33 +0800

    53-最大子序列和

--- 动态规划/最大子序和-53.js
+++ 动态规划/最大子序和-53.js
@@ -1,18 +1,59 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * https://leetcode-cn.com/problems/maximum-subarray/submissions/
+ * https://leetcode-cn.com/problems/maximum-subarray/solution/hua-jie-suan-fa-53-zui-da-zi-xu-he-by-guanpengchn/
  */
-let maxSubArray = function(nums) {
-  let n = nums.length;
-  let dp = [];
+let maxSubArray = function (nums) {
+  if (nums.length === 1) return nums[0]
 
-  dp[n - 1] = nums[n - 1];
+  let max = nums[0]
+  let dp = []
+  dp[0] = nums[0]
 
-  for (let i = n - 2; i >= 0; i--) {
-    let pickSelf = nums[i];
-    let pickWithNext = pickSelf + dp[i + 1];
-    dp[i] = Math.max(pickSelf, pickWithNext);
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
+    max = Math.max(max, dp[i])
   }
 
-  return Math.max(...dp);
-};
+  return max
+}
+
+// 暴力法
+// 直接三次嵌套循环
+// 记住正负无穷书 Infinity
+var maxSubArray1 = function (nums) {
+  if (nums.length === 1) return nums[0]
+
+  let max = -Infinity
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j <= nums.length; j++) {
+      const sum = nums.slice(i, j + 1).reduce((a, b) => a + b)
+      max = Math.max(sum, max)
+    }
+  }
+
+  return max
+}
+
+// 前缀和
+// 不一定每次都要计算出来，可以使用前缀和
+var maxSubArray2 = function (nums) {
+  if (nums.length === 1) return nums[0]
+
+  let max = -Infinity
+
+  let sum = 0
+  for (let i = 0; i < nums.length; i++) {
+    sum = 0
+    for (let j = i; j <= nums.length; j++) {
+      sum = sum + nums[j]
+      if (sum > max) {
+        max = sum
+      }
+    }
+  }
+
+  return max
+}
+
+//
