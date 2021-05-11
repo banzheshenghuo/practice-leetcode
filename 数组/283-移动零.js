4ff28453570b064bf966a93e3fb4021d3113e19a
shuang.tu@forceclouds.com <2271362435@qq.com>
2021-05-11 17:41:45 +0800

    283 移动零

--- 数组/283-移动零.js
+++ 数组/283-移动零.js
@@ -30,6 +30,23 @@ var moveZeroes = function (nums) {
   }
 }
 
+// 相对方案一的升级版，只需要一次循环
+var moveZeroes1 = function (nums) {
+  let len = nums.length
+  let i = 0
+
+  while (i < len) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1)
+      nums.push(0)
+      len--
+    } else {
+      i++
+    }
+  }
+  return nums
+}
+
 // 利用快排的思想
 var moveZeroes2 = function (nums) {
   if (nums.length <= 1) return
@@ -45,4 +62,24 @@ var moveZeroes2 = function (nums) {
       }
     }
   }
+}
+
+// 快慢指针或者叫读写指针，比上面好理解些。
+// fast是读指针，slow是写指针，当读指针不为0时，赋值给写指针
+var moveZeroes3 = function (nums) {
+  if (!nums.length) return nums
+
+  let s = 0
+  for (let f = 0; f < nums.length; f++) {
+    if (nums[f] !== 0) {
+      nums[s] = nums[f]
+      s++
+    }
+  }
+
+  for (; s < nums.length; s++) {
+    nums[s] = 0
+  }
+
+  return nums
 }
